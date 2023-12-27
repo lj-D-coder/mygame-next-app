b42b9150fbd406b3d6b0835fe25cd80ed6831cae
lj-D-coder <lj_1990@live.com>
2023-12-27 14:05:46 +0530

    fist commit

---
+++ app/TicketPage/[id]/page.jsx
@@ -0,0 +1,29 @@
+import TicketForm from "@/app/(components)/TicketForm";
+
+const getTicketByID = async (id) => {
+  const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+    cache: "no-cache",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to get ticket.");
+  }
+  return res.json();
+};
+
+const TicketPage = async ({ params }) => {
+  const EDITMODE = params.id === "new" ? false : true;
+  let updateTicketData = {};
+  if (EDITMODE) {
+    updateTicketData = await getTicketByID(params.id);
+    updateTicketData = updateTicketData.foundTicket;
+  } else { 
+    updateTicketData = {
+      _id: "new"
+    }
+  }
+
+  return <TicketForm ticket={updateTicketData } />
+  
+}
+
+export default TicketPage
