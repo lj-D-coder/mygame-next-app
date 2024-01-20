448093d60e65c27e825554b5958975c7636f3c91
LeonardChongtham <sixpathssenin@gmail.com>
2024-01-20 19:11:49 +0530

    Business crud update

--- app/api/business/setup/[businessID]/route.js
+++ app/api/business/setup/[businessID]/route.js
@@ -20,21 +20,23 @@ export async function DELETE(req, { params }) {
         const businessData = await BusinessSetup.findOne({ businessID });
         await BusinessSetup.findByIdAndDelete(businessData._id);
         await Users.findByIdAndDelete(businessID);
-        return NextResponse.json({ message: "Business Data Deleted" }, { status: 200 });
+        return NextResponse.json({status: 200, message: "Business Data Deleted" }, { status: 200 });
     } catch (error) {
-        return NextResponse.json({ message: "Error", error }, { status: 500 });
+        return NextResponse.json({status: 500, message: "Error", error }, { status: 500 });
     }
 }
 
 export async function PUT(req, { params }) { 
+
+    console.log(params);
     try {
         const { businessID } = params;
-        const body = await req.json();
-        const newData = body.formData;
+        const newData = await req.json();
+        //const newData = body.formData;
         const findId = await BusinessSetup.findOne({ businessID });
-        const updateTicketData = await BusinessSetup.findByIdAndUpdate(findId._id, { ...newData, });
-        return NextResponse.json({ message: "Data Updated" }, { status: 200 });
+        const updateTicketData = await BusinessSetup.findByIdAndUpdate(findId._id, { ...newData});
+        return NextResponse.json({status: 200 , message: "Data Updated" },{ status: 200 });
     } catch (error) {
-        return NextResponse.json({ message: "Error", error }, { status: 500 });
+        return NextResponse.json({  status: 500, message: "Error", error }, { status: 500 });
     }
 }
