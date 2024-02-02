2cb8e8b184ff58380be02a0f84a2b6d7424d07fc
Jake L <143929593+lj-D-coder@users.noreply.github.com>
2024-02-02 16:36:15 +0530

    stable firestore connection

--- app/api/client/booking/confirmation/route.js
+++ app/api/client/booking/confirmation/route.js
@@ -1,78 +1,47 @@
 // pages/api/addUser.js
-import db_firestore from '@/lib/firebase/admin';
-import { NextResponse } from 'next/server';
+import firestore_db from "@/lib/firebase/admin";
+import { NextResponse } from "next/server";
+import connection from "@/lib/utils/db-connect";
+import MatchModel from "@/app/(models)/MatchModel";
+import BookingModel from "@/app/(models)/BookingModel";
+import removeProperties from "@/lib/utils/delete-keys";
 
-export async function POST(req) { 
-  const { name, email } = await req.json(); // Parse the request body as JSON
+export async function POST(req) {
+  await connection();
+  try {
+    const { bookingId } = await req.json();
+    const booking = await BookingModel.findById(bookingId);
+    const data = await MatchModel.findById(booking.matchId);
 
-    await db_firestore.collection('users').add({
-      name,
-      email,
-    });
-  return NextResponse.json({ message: 'User added successfully' }, { status: 201 })
+    // Convert the data to a plain JavaScript object
+    let dataToSave = data.toObject();
+
+    dataToSave = await removeProperties(dataToSave, ["_id", "__v"]);
+
+    // Convert teams to plain JavaScript objects
+    for (let team in dataToSave.teams) {
+      let teamData = {};
+      for (let key in dataToSave.teams[team]) {
+        teamData[key] = [...dataToSave.teams[team][key]];
+      }
+      dataToSave.teams[team] = Object.fromEntries(dataToSave.teams[team]);
+    }
+
+    const docRef = firestore_db.doc("matchesCollection/" + booking.matchId);
+    const docSnap = await docRef.get();
+
+    if (!docSnap.exists) {
+      await docRef.set(dataToSave);
+    } else {
+      await docRef.update(dataToSave);
+    }
+
+    console.log(`Document updated with ID: ${docRef.id}`);
+
+    // Return the document ID as a response
+    return NextResponse.json({ message: "Success" }, { status: 201 });
+  } catch (error) {
+    // Handle any errors
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// import firebaseApp from "@/lib/firebase/config";
-// import { NextResponse } from 'next/server';
-// import { getFirestore, doc, getDoc, setDoc, updateDoc } from "firebase/firestore"; 
-// import connection from "@/lib/utils/db-connect";
-// import MatchModel from "@/app/(models)/MatchModel";
-// import BookingModel from "@/app/(models)/BookingModel";
-// import removeProperties from "@/lib/utils/delete-keys";
-
-// const firestore_db = getFirestore(firebaseApp); // Use getFirestore
-
-// export async function POST(req) {
-//   await connection();
-//   try {
-//     const { bookingId } = await req.json();
-//     const booking = await BookingModel.findById(bookingId);
-//     const data = await MatchModel.findById(booking.matchId);
-
-//     // Convert the data to a plain JavaScript object
-//     let dataToSave = data.toObject();
-
-//     // Convert _id to a string
-//     //dataToSave._id = dataToSave._id.toString();
-    
-//     dataToSave = await removeProperties(dataToSave, ['_id', '__v']);
-    
-
-//     // Convert teams to plain JavaScript objects
-//     for (let team in dataToSave.teams) {
-//       let teamData = {};
-//       for (let key in dataToSave.teams[team]) {
-//         teamData[key] = [...dataToSave.teams[team][key]];
-//       }
-//       dataToSave.teams[team] = Object.fromEntries(dataToSave.teams[team]);
-//     }
-
-//     const docRef = doc(firestore_db, 'matchesCollection', booking.matchId);
-//     const docSnap = await getDoc(docRef);
-
-//     if (!docSnap.exists()) {
-//       await setDoc(docRef, dataToSave);
-//     } else {
-//       await updateDoc(docRef, dataToSave);
-//     }
-
-//     console.log(`Document updated with ID: ${docRef.id}`);
-
-//     // Return the document ID as a response
-//     return NextResponse.json({ id: docRef.id, docRef }, { status: 201 })
-//   } catch (error) {
-//     // Handle any errors
-//     return NextResponse.json({ error: error.message }, { status: 500 })
-//   }
-// }
