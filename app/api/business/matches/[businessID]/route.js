a1200537cfaa28c46a914d389582b344f9612cce
Jake L <143929593+lj-D-coder@users.noreply.github.com>
2024-02-18 18:19:21 +0530

    add pricing

--- app/api/business/matches/[businessID]/route.js
+++ app/api/business/matches/[businessID]/route.js
@@ -1,8 +1,8 @@
-import BusinessSetup from "@/app/(models)/BusinessSetup";
 import { NextResponse } from "next/server";
 import connection from "@/lib/utils/db-connect";
 import MatchModel from "@/app/(models)/MatchModel";
 import Users from "@/app/(models)/Users";
+import PricingModel from "@/app/(models)/PricingModel";
 
 export async function GET(req, { params }) {
   await connection();
@@ -19,6 +19,14 @@ export async function GET(req, { params }) {
         message: "invalid business ID",
       });
     }
+    const individualPrice = await PricingModel.findOne({ businessID: businessID }).select('price.individual.Price');
+    if (!individualPrice) {
+      return NextResponse.json({
+        status: 404,
+        success: false,
+        message: "Price data not found",
+      });
+    }
     let today = new Date();
     today.setHours(0, 0, 0, 0);
 
@@ -30,7 +38,11 @@ export async function GET(req, { params }) {
 
     const allMatches = matches.map(match => {
       const { _id: matchId, ...rest } = match.toObject();
-      return { matchId, ...rest };
+      return {
+        matchId,
+        "price": individualPrice.price.individual.Price,
+        ...rest
+      };
      });
 
     return NextResponse.json({
