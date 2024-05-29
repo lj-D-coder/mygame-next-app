2ea0cbed5040f0ae44f6d78db1e1d93f01f23086
Jake L <143929593+lj-D-coder@users.noreply.github.com>
2024-05-29 19:00:18 +0530

    user location update

--- app/api/client/update-location/route.js
+++ app/api/client/update-location/route.js
@@ -1,29 +1,45 @@
 import { NextResponse } from "next/server";
 import connection from "@/lib/utils/db-connect";
 import Users from "@/app/(models)/Users";
+import LocationModel from "@/app/(models)/locationService";
 
 export async function POST(req) {
   await connection();
   try {
     const { userId, longitude, latitude } = await req.json();
-
-    const result = await Users.findOneAndUpdate(
-      { _id: userId }, // find a document with this filter
-      {
-        $set: {
-          // fields to update
-          userLocation: {
-            type: "Point",
-            coordinates: [longitude, latitude],
+     
+    const user = await Users.findById(userId);
+    if (!user) {
+      return NextResponse.json({
+        status: 404,
+        success: false,
+        message: "user not Found",
+      });
+    }
+    
+      const updatedLocation = await LocationModel.findOneAndUpdate(
+        { _id: userId }, // find a document with this filter
+        {
+          $setOnInsert: {
+            // fields to insert if the document doesn't exist
+            _id: userId,
+            "name": user.userName,
+          },
+          $set: {
+            // fields to update
+            "location": {
+              type: "Point",
+              coordinates: [longitude, latitude], 
+            },
           },
         },
-      },
-      {
-        upsert: true, // if no documents match the filter, create a new one
-        new: true, // return the updated document
-      }
-    );
-    if (result) {
+        {
+          upsert: true, // if no documents match the filter, create a new one
+          new: true, // return the updated document
+        }
+      );
+
+    if (updatedLocation) {
       return NextResponse.json({
         status: 200,
         success: true,
